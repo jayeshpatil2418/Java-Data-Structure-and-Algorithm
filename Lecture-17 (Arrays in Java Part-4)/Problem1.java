17e443bb4ac57bde2cf78d2664e7c00bda2b9522
Jayesh Patil <jayeshpatil2418@gmail.com>
2025-01-21 22:58:34 +0530

    Add files via upload

--- Lecture-17 (Arrays in Java Part-4)/Problem1.java
+++ Lecture-17 (Arrays in Java Part-4)/Problem1.java
@@ -1,1 +1,23 @@
+class Problem1{
+    //Swap Two Numbers using temp variable. 
+    static void swapNumbers(int a , int b){
+        System.out.println("Original Values Before Swapping");
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
 
+        
+        int temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("Values After Swapping");
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+    }
+
+    public static void main(String[] args){
+        int a =3;
+        int b =9;
+        swapNumbers(a, b);
+    }
+}
