4ba7c89af2307192f30661a37c7af703b02537c8
Jayesh Patil <jayeshpatil2418@gmail.com>
2025-01-23 20:52:59 +0530

    Add files via upload

--- Lecture-18 (Arrays in Java Part-5)/Problem1.java
+++ Lecture-18 (Arrays in Java Part-5)/Problem1.java
@@ -1,1 +1,40 @@
+import java.util.Scanner;
+class Problem1{
+    //Sort an array only consisting of 0s and 1s.
+    static void sortZeroAndOnes(int arr[]){
+        int count=0;
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]==0){
+                count++;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(i<count){
+                arr[i]=0;
+            }
+            else{
+                arr[i]=1;
+            }
+        }
+    }
 
+    static void printArray(int arr[]){
+        for(int i =0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length of array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter "+n+" elements of array: ");
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        sortZeroAndOnes(arr);
+        System.out.println("Sorted Array: ");
+        printArray(arr);
+        sc.close();
+    }
+}
