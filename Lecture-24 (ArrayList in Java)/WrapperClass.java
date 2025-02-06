863bd022d0d7de3272e46999b172bb781fa98f32
Jayesh Patil <jayeshpatil2418@gmail.com>
2025-02-06 23:33:13 +0530

    Add files via upload

---
+++ Lecture-24 (ArrayList in Java)/WrapperClass.java
@@ -0,0 +1,9 @@
+class WrapperClass{
+    public static void main(String[] args) {
+        Integer i =Integer.valueOf(1);
+        System.out.println(i);
+
+        Float f = Float.valueOf(2.9f);
+        System.out.println(f);
+    }
+}
